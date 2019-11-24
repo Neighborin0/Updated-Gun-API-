8fc071b64f15339a7dc4537f34392d7f113c4690
Neighborin0 <58134925+Neighborin0@users.noreply.github.com>
2019-11-24 02:31:50 -0800

    Add files via upload

---
+++ ModTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using Gungeon;
+using MonoMod;
+using UnityEngine;
+using ItemAPI;
+using System.Collections.Generic;
+
+namespace FrostAndGunfireItems
+{
+   
+    public class ModTest : ETGModule
+    {
+     
+        public override void Init()
+        {
+           
+        
+            
+        }
+
+      
+        public override void Start()
+        {
+            FakePrefabHooks.Init();
+            ItemBuilder.Init();
+            BasicGun.Add();
+            ETGModConsole.Log("shit", false);
+
+
+
+
+
+
+        }
+
+      
+        public override void Exit()
+        {
+           
+        }
+    }
+}
