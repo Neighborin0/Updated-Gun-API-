c7f3c7fe3d5a8acc8b10416cbc559d7896766ee9
Neighborin0 <58134925+Neighborin0@users.noreply.github.com>
2020-06-05 08:11:02 -0700

    Add files via upload

---
+++ Tools.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+
+using UnityEngine;
+using Dungeonator;
+using MonoMod.RuntimeDetour;
+
+namespace GungeonAPI
+{
+    //Utility methods
+    public static class Tools
+    {
+        public static bool verbose = true;
+        private static string defaultLog = Path.Combine(ETGMod.ResourcesDirectory, "FAGF.txt");
+        public static string modID = "FAGF";
+
+        private static Dictionary<string, float> timers = new Dictionary<string, float>();
+
+        public static void Init()
+        {
+            if (File.Exists(defaultLog)) File.Delete(defaultLog);
+        }
+
+        public static void Print<T>(T obj, string color = "FFFFFF", bool force = false)
+        {
+            if (verbose || force)
+            {
+                string[] lines = obj.ToString().Split('\n');
+                foreach (var line in lines)
+                    LogToConsole($"<color=#{color}>[{modID}] {line}</color>");
+            }
+
+            Log(obj.ToString());
+        }
+
+        public static tk2dSpriteDefinition CopyDefinitionFrom(this tk2dSpriteDefinition other)
+        {
+            return new tk2dSpriteDefinition
+            {
+                boundsDataCenter = other.boundsDataCenter,
+                boundsDataExtents = other.boundsDataExtents,
+                colliderConvex = other.colliderConvex,
+                colliderSmoothSphereCollisions = other.colliderSmoothSphereCollisions,
+                colliderType = other.colliderType,
+                colliderVertices = other.colliderVertices,
+                collisionLayer = other.collisionLayer,
+                complexGeometry = other.complexGeometry,
+                extractRegion = other.extractRegion,
+                flipped = other.flipped,
+                indices = other.indices,
+                material = new Material(other.material),
+                materialId = other.materialId,
+                materialInst = new Material(other.materialInst),
+                metadata = other.metadata,
+                name = other.name,
+                normals = other.normals,
+                physicsEngine = other.physicsEngine,
+                position0 = other.position0,
+                position1 = other.position1,
+                position2 = other.position2,
+                position3 = other.position3,
+                regionH = other.regionH,
+                regionW = other.regionW,
+                regionX = other.regionX,
+                regionY = other.regionY,
+                tangents = other.tangents,
+                texelSize = other.texelSize,
+                untrimmedBoundsDataCenter = other.untrimmedBoundsDataCenter,
+                untrimmedBoundsDataExtents = other.untrimmedBoundsDataExtents,
+                uvs = other.uvs
+            };
+        }
+
+        public static void SetProjectileSpriteRight(this Projectile proj, string name, int pixelWidth, int pixelHeight, int? overrideColliderPixelWidth = null, int? overrideColliderPixelHeight = null)
+        {
+            try
+            {
+                bool flag = overrideColliderPixelWidth == null;
+                if (flag)
+                {
+                    overrideColliderPixelWidth = new int?(pixelWidth);
+                }
+                bool flag2 = overrideColliderPixelHeight == null;
+                if (flag2)
+                {
+                    overrideColliderPixelHeight = new int?(pixelHeight);
+                }
+                float num = (float)pixelWidth / 16f;
+                float num2 = (float)pixelHeight / 16f;
+                float x = (float)overrideColliderPixelWidth.Value / 16f;
+                float y = (float)overrideColliderPixelHeight.Value / 16f;
+                proj.GetAnySprite().spriteId = ETGMod.Databases.Items.ProjectileCollection.inst.GetSpriteIdByName(name);
+                tk2dSpriteDefinition tk2dSpriteDefinition = ETGMod.Databases.Items.ProjectileCollection.inst.spriteDefinitions[(PickupObjectDatabase.GetById(12) as Gun).DefaultModule.projectiles[0].GetAnySprite().spriteId].CopyDefinitionFrom();
+                tk2dSpriteDefinition.boundsDataCenter = new Vector3(num / 2f, num2 / 2f, 0f);
+                tk2dSpriteDefinition.boundsDataExtents = new Vector3(num, num2, 0f);
+                tk2dSpriteDefinition.untrimmedBoundsDataCenter = new Vector3(num / 2f, num2 / 2f, 0f);
+                tk2dSpriteDefinition.untrimmedBoundsDataExtents = new Vector3(num, num2, 0f);
+                tk2dSpriteDefinition.position0 = new Vector3(0f, 0f, 0f);
+                tk2dSpriteDefinition.position1 = new Vector3(0f + num, 0f, 0f);
+                tk2dSpriteDefinition.position2 = new Vector3(0f, 0f + num2, 0f);
+                tk2dSpriteDefinition.position3 = new Vector3(0f + num, 0f + num2, 0f);
+                tk2dSpriteDefinition.colliderVertices[1].x = x;
+                tk2dSpriteDefinition.colliderVertices[1].y = y;
+                tk2dSpriteDefinition.name = name;
+                ETGMod.Databases.Items.ProjectileCollection.inst.spriteDefinitions[proj.GetAnySprite().spriteId] = tk2dSpriteDefinition;
+                proj.baseData.force = 0f;
+            }
+            catch (Exception ex)
+            {
+                ETGModConsole.Log("Ooops! Seems like something got very, Very, VERY wrong. Here's the exception:", false);
+                ETGModConsole.Log(ex.ToString(), false);
+            }
+        }
+        public static void PrintRaw<T>(T obj, bool force = false)
+        {
+            if (verbose || force)
+                LogToConsole(obj.ToString());
+
+            Log(obj.ToString());
+        }
+
+        public static void PrintError<T>(T obj, string color = "FF0000")
+        {
+            string[] lines = obj.ToString().Split('\n');
+            foreach (var line in lines)
+                LogToConsole($"<color=#{color}>[{modID}] {line}</color>");
+
+            Log(obj.ToString());
+        }
+
+        public static void PrintException(Exception e, string color = "FF0000")
+        {
+            string message = e.Message + "\n" + e.StackTrace;
+            {
+                string[] lines = message.Split('\n');
+                foreach (var line in lines)
+                    LogToConsole($"<color=#{color}>[{modID}] {line}</color>");
+            }
+
+            Log(e.Message);
+            Log("\t" + e.StackTrace);
+        }
+
+        public static void Log<T>(T obj)
+        {
+            using (StreamWriter writer = new StreamWriter(Path.Combine(ETGMod.ResourcesDirectory, defaultLog), true))
+            {
+                writer.WriteLine(obj.ToString());
+            }
+        }
+
+        public static void Log<T>(T obj, string fileName)
+        {
+            if (!verbose) return;
+            using (StreamWriter writer = new StreamWriter(Path.Combine(ETGMod.ResourcesDirectory, fileName), true))
+            {
+                writer.WriteLine(obj.ToString());
+            }
+        }
+
+        public static void LogToConsole(string message)
+        {
+            message.Replace("\t", "    ");
+            ETGModConsole.Log(message);
+        }
+
+        private static void BreakdownComponentsInternal(this GameObject obj, int lvl = 0)
+        {
+            string space = "";
+            for (int i = 0; i < lvl; i++)
+            {
+                space += "\t";
+            }
+
+            Log(space + obj.name + "...");
+            foreach (var comp in obj.GetComponents<Component>())
+            {
+                Log(space + "    -" + comp.GetType());
+            }
+
+            foreach (var child in obj.GetComponentsInChildren<Transform>())
+            {
+                if (child != obj.transform)
+                    child.gameObject.BreakdownComponentsInternal(lvl + 1);
+            }
+        }
+
+        public static void BreakdownComponents(this GameObject obj)
+        {
+            BreakdownComponentsInternal(obj, 0);
+        }
+
+        public static void ExportTexture(Texture texture, string folder = "")
+        {
+            string path = Path.Combine(ETGMod.ResourcesDirectory, folder);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            File.WriteAllBytes(Path.Combine(path, texture.name + ".png"), ((Texture2D)texture).EncodeToPNG());
+        }
+
+        public static T GetEnumValue<T>(string val) where T : Enum
+        {
+            return (T)Enum.Parse(typeof(T), val.ToUpper());
+        }
+
+        public static void LogPropertiesAndFields<T>(T obj, string header = "")
+        {
+            Log(header);
+            Log("=======================");
+            if (obj == null) { Log("LogPropertiesAndFields: Null object"); return; }
+            Type type = obj.GetType();
+            Log($"Type: {type}");
+            PropertyInfo[] pinfos = type.GetProperties();
+            Log($"{typeof(T)} Properties: ");
+            foreach (var pinfo in pinfos)
+            {
+                try
+                {
+                    var value = pinfo.GetValue(obj, null);
+                    string valueString = value.ToString();
+                    bool isList = obj?.GetType().GetGenericTypeDefinition() == typeof(List<>);
+                    if (isList)
+                    {
+                        var list = value as List<object>;
+                        valueString = $"List[{list.Count}]";
+                        foreach (var subval in list)
+                        {
+                            valueString += "\n\t\t" + subval.ToString();
+                        }
+                    }
+                    Log($"\t{pinfo.Name}: {valueString}");
+                }
+                catch { }
+            }
+            Log($"{typeof(T)} Fields: ");
+            FieldInfo[] finfos = type.GetFields();
+            foreach (var finfo in finfos)
+            {
+                Log($"\t{finfo.Name}: {finfo.GetValue(obj)}");
+            }
+        }
+
+    }
+}
